054e56435ca06d156e84767c692bc3e6ce4ca918
Sadrac Tijerina <reavity3@gmail.com>
2022-08-05 22:56:09 -0400

    Got date to print on application

--- app/src/main/java/sadrac/tijerina/ageinminutes/MainActivity.kt
+++ app/src/main/java/sadrac/tijerina/ageinminutes/MainActivity.kt
@@ -4,15 +4,21 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
 
         btnDatePicker.setOnClickListener {
             clickDatePicker()
@@ -26,8 +32,14 @@ class MainActivity : AppCompatActivity() {
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
-            { view, year, month, dayOfMonth ->
-                Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                val selectedDate = "${selectedMonth + 1}/$selectedDayOfMonth/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("MM/dd/yyyy", Locale.US)
+
+                val date = sdf.parse(selectedDate)
             },
             year,
             month,
